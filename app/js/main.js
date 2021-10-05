2508963060d70f6ac219a2d36aa61340f38fc49c
haaag <haag_@tutanota.com>
2021-10-05 16:58:11 -0300

    added try catch to main function

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,19 @@
-import { getFormData, validateFormData, userData, userLoop } from './functions.js'
-
-const moduleName = 'Deals'
+import { validateFormData, userData, userLoop } from './functions.js'
+import { getFormData, moduleName } from './crm_functions.js'
 
 ZOHO.embeddedApp.init().then(function () {
   swal('Iniciando Widget', { buttons: false, timer: 1500 })
-  setTimeout(() => {
-    getFormData(moduleName, userData).then((formData) => {
-      validateFormData(formData).then((proceed) => {
-        if (proceed) {
-          userLoop()
-        }
+  try {
+    setTimeout(() => {
+      getFormData(moduleName, userData).then((formData) => {
+        validateFormData(formData).then((proceed) => {
+          if (proceed) {
+            userLoop()
+          }
+        })
       })
-    })
-  }, 1100)
+    }, 1100)
+  } catch (err) {
+    console.error(err)
+  }
 })
